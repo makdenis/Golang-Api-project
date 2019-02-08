169a5578c1e3709f51ef2c41a023f343abd4cd2b
makdenis <makdenis.1997@gmail.com>
2019-02-08 14:05:08 +0300

    Update vote.go

--- controllers/vote.go
+++ controllers/vote.go
@@ -11,18 +11,12 @@ import (
 )
 
 func Vote(Db *sql.DB, respWriter http.ResponseWriter, request *http.Request) {
-	//fmt.Println(Db)
 	respWriter.Header().Set("Content-Type", "application/json; charset=utf-8")
-
 	slug := mux.Vars(request)["slug"]
-	//fmt.Println(request)
-
 	vote := Models.Vote{}
-	//
 	if err := json.NewDecoder(request.Body).Decode(&vote); err != nil {
 		panic(err)
 	}
-	//user.Nickname = nickname
 	id,_:=strconv.Atoi(slug)
 	user:=GetUsersByEmailOrNick(Db,"",vote.NickName)
 	if len(user)==0{
@@ -32,9 +26,6 @@ func Vote(Db *sql.DB, respWriter http.ResponseWriter, request *http.Request) {
 		return
 	}
 	threads:=GetThreadBySlugorID(Db,slug,id)
-		//res, checkThreads, forumName:= AddThread(Db, thread.Slug,thread.Author,slug, respWriter)
-	//forum.User=checkuser.Nickname
-	//fmt.Println(res)
 	if len(threads)==0{
 		respWriter.WriteHeader(http.StatusNotFound)
 		tmp2:=errr{"Can't find user with nickname: "+slug}
@@ -44,7 +35,6 @@ func Vote(Db *sql.DB, respWriter http.ResponseWriter, request *http.Request) {
 		var count int
 		var flag bool
 		insertUserQuery := `insert into votes (username, thread, voice) values ($1, $2, $3)`
-
 		_, err:= Db.Exec(insertUserQuery, vote.NickName,threads[0].ID,vote.Voice)
 		if err!=nil {
 			fmt.Println(err)
@@ -58,16 +48,9 @@ func Vote(Db *sql.DB, respWriter http.ResponseWriter, request *http.Request) {
 			if errr!=nil{
 				fmt.Println(errr)}
 		}
-
-
 		if vote.Voice<0 {
-
 			votes:=GetVoteByUser(Db,vote.NickName)
-
 			if len(votes)>0{
-				//if votes[0].Voice==-1{
-				//	count=0
-				//}else{
 				count=2
 			}else{
 				count=1
@@ -75,35 +58,24 @@ func Vote(Db *sql.DB, respWriter http.ResponseWriter, request *http.Request) {
 			insertUserQuery := `update threads set votes=votes-$3 where LOWER(slug)=lower($1) or id = $2;`
 			threads[0].Votes-=count
 			_, _ = Db.Exec(insertUserQuery, slug,id,count)
-
 		}}
-
 		respWriter.WriteHeader(http.StatusOK)
-
 		writeJSONBody(&respWriter, threads[0])
 	}}
 func GetVoteByUser(Db *sql.DB, user string) []Models.Vote {
 	votes := make([]Models.Vote, 0)
 	query:="SELECT username::text, voice::integer FROM votes WHERE LOWER(username) = LOWER($1)"
-
 	var resultRows *sql.Rows
-
-
-	//fmt.Println(query)
 	resultRows,err:= Db.Query(query, user)
 	if err!=nil{
 		fmt.Println(err)}
-	//fmt.Println(err)
 	defer resultRows.Close()
-
 	for resultRows.Next() {
 		vote := new(Models.Vote)
 		err := resultRows.Scan(&vote.NickName, &vote.Voice)
 		if err != nil {	}
-
 		votes = append(votes, *vote)
 	}
-
 	return votes
 }
 
@@ -114,29 +86,16 @@ func GetThreadDetails(Db *sql.DB,  respWriter http.ResponseWriter, request *http
 	slug := mux.Vars(request)["slug"]
 	id,_:=strconv.Atoi(slug)
 	var resultRows *sql.Rows
-
-
-
-	//fmt.Println(query)
 	resultRows,_= Db.Query(query, slug, id)
-
-
-
-
-
-	//fmt.Println(err)
 	defer resultRows.Close()
-
 	for resultRows.Next() {
 		thread := new(Models.Thread)
 		err := resultRows.Scan(&thread.Author, &thread.Created, &thread.Forum, &thread.ID, &thread.Message, &thread.Slug, &thread.Title, &thread.Votes)
 		if err != nil {	}
-
 		thr = append(thr, *thread)
 	}
 	if len(thr)>0 {
 		respWriter.WriteHeader(http.StatusOK)
-
 		writeJSONBody(&respWriter, thr[0])
 	}else{
 		respWriter.WriteHeader(http.StatusNotFound)

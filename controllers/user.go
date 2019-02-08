9513e12322c196eb9f6cab2c7baf3f236dfd91ea
makdenis <makdenis.1997@gmail.com>
2019-02-08 14:03:57 +0300

    Update user.go

--- controllers/user.go
+++ controllers/user.go
@@ -13,23 +13,16 @@ type errr struct{
 	Message string `json:"message"`
 }
 func CreateUser(Db *sql.DB, respWriter http.ResponseWriter, request *http.Request) {
-	//fmt.Println(Db)
 	respWriter.Header().Set("Content-Type", "application/json; charset=utf-8")
-
 	nickname := mux.Vars(request)["nickname"]
-	//fmt.Println(request)
 	user := Models.User{}
-
 	if err := json.NewDecoder(request.Body).Decode(&user); err != nil {
 		panic(err)
 	}
 	user.Nickname = nickname
-
 	res, users := AddUser(Db, &user)
-	//fmt.Println(res)
 	if res {
 		insertUserQuery := `insert into users (nickname, about, email, fullname) values ($1, $2, $3, $4);`
-
 		_, _ = Db.Exec(insertUserQuery, user.Nickname, user.About, user.Email, user.Fullname)
 		respWriter.WriteHeader(http.StatusCreated)
 		writeJSONBody(&respWriter, user)
@@ -41,37 +34,28 @@ func CreateUser(Db *sql.DB, respWriter http.ResponseWriter, request *http.Reques
 
 func GetUsersByEmailOrNick(Db *sql.DB, email, nickname string) []Models.User {
 	users := make([]Models.User, 0)
-
 	query := "SELECT about::text, email::text, fullname::text, nickname::text FROM users WHERE LOWER(email) = LOWER($1) OR LOWER(nickname) = LOWER($2)"
-
 	resultRows, _ := Db.Query(query, email, nickname)
 	defer resultRows.Close()
-
 	for resultRows.Next() {
 		user := new(Models.User)
 		err := resultRows.Scan(&user.About, &user.Email, &user.Fullname, &user.Nickname)
 		if err != nil {
 			panic(err)
 		}
-
 		users = append(users, *user)
 	}
-
 	return users
 }
 
 func AddUser(Db *sql.DB, user *Models.User) (bool, []Models.User) {
-
 	conflictUsers := GetUsersByEmailOrNick(Db, user.Email, user.Nickname)
-
 	if len(conflictUsers) == 2 && conflictUsers[0] == conflictUsers[1] {
 		conflictUsers = conflictUsers[:1]
 	}
-
 	if len(conflictUsers) > 0 {
 		return false, conflictUsers
 	}
-
 	return true, nil
 }
 
@@ -82,14 +66,9 @@ func writeJSONBody(respWriter *http.ResponseWriter, v interface{}) {
 }
 
 func GetUser(Db *sql.DB, respWriter http.ResponseWriter, request *http.Request) {
-
 	respWriter.Header().Set("Content-Type", "application/json; charset=utf-8")
-
 	nickname := mux.Vars(request)["nickname"]
-	//fmt.Println(nickname)
-	//fmt.Println("get")
 	user := make([]Models.User, 0)
-
 	user=GetUsersByEmailOrNick(Db,"", nickname)
 	if len(user)==0{
 		respWriter.WriteHeader(http.StatusNotFound)
@@ -102,29 +81,13 @@ func GetUser(Db *sql.DB, respWriter http.ResponseWriter, request *http.Request) 
 }
 
 
-
-
 func UpdateUser(Db *sql.DB, respWriter http.ResponseWriter, request *http.Request) {
-	//fmt.Println("ppp")
 	respWriter.Header().Set("Content-Type", "application/json; charset=utf-8")
-
 	nickname := mux.Vars(request)["nickname"]
-
-	//fmt.Println(request)
 	user := Models.User{}
-
 	if err := json.NewDecoder(request.Body).Decode(&user); err != nil {
 		panic(err)
 	}
-
-
-
-
-
-
-
-
-
 	user.Nickname = nickname
 	conflictUsers := GetUsersByEmailOrNick(Db, "", user.Nickname)
 	if len(conflictUsers) == 0 {
@@ -133,8 +96,6 @@ func UpdateUser(Db *sql.DB, respWriter http.ResponseWriter, request *http.Reques
 		writeJSONBody(&respWriter, tmp)
 		return
 	}
-//
-// fmt.Println(user.Email)
 	conflictEmails := GetUsersByEmailOrNick(Db, user.Email,"")
 	if len(conflictEmails) != 0 {
 		respWriter.WriteHeader(http.StatusConflict)
@@ -142,7 +103,6 @@ func UpdateUser(Db *sql.DB, respWriter http.ResponseWriter, request *http.Reques
 		writeJSONBody(&respWriter, tmp)
 		return
 	}
-
 	olduser:=GetUsersByEmailOrNick(Db,"", nickname)
 	if user.Fullname==""{
 		user.Fullname=olduser[0].Fullname
@@ -153,17 +113,13 @@ func UpdateUser(Db *sql.DB, respWriter http.ResponseWriter, request *http.Reques
 	if user.Email==""{
 		user.Email=olduser[0].Email
 	}
-	//res, users := AddUser(Db, &user)
-	//fmt.Println(user)
-
-		updateUserQuery := `UPDATE users set about =$1, email = $2, fullname = $3  where lower (nickname)=lower ($4);`
-
-		_, _ = Db.Exec(updateUserQuery,  user.About, user.Email, user.Fullname, user.Nickname)
-		respWriter.WriteHeader(http.StatusOK)
-		writeJSONBody(&respWriter, user)
+	updateUserQuery := `UPDATE users set about =$1, email = $2, fullname = $3  where lower (nickname)=lower ($4);`
+	_, _ = Db.Exec(updateUserQuery,  user.About, user.Email, user.Fullname, user.Nickname)
+	respWriter.WriteHeader(http.StatusOK)
+	writeJSONBody(&respWriter, user)
 	}
+
 func GetSortedUsers(Db *sql.DB, respWriter http.ResponseWriter, request *http.Request) {
-	//fmt.Println(Db)
 	respWriter.Header().Set("Content-Type", "application/json; charset=utf-8")
 	var limit string
 	var since string
@@ -172,10 +128,8 @@ func GetSortedUsers(Db *sql.DB, respWriter http.ResponseWriter, request *http.Re
 	if (request.URL.Query()["limit"] != nil) {
 		limit = request.URL.Query()["limit"][0]
 	}
-
 	if (request.URL.Query()["since"] != nil) {
 		since = request.URL.Query()["since"][0]
-
 	}
 	if (request.URL.Query()["desc"] != nil) {
 		if request.URL.Query()["desc"][0] == "true" {
@@ -184,9 +138,7 @@ func GetSortedUsers(Db *sql.DB, respWriter http.ResponseWriter, request *http.Re
 			desc = false
 		}
 	}
-	//}
 	users := make([]Models.User, 0)
-	//nickname::text, about::text, email::text, fullname::text  from
 	query := `SELECT about, fullname, email, nickname  FROM (
     SELECT about, fullname, email, nickname  FROM users AS u1
     JOIN threads AS t
@@ -197,15 +149,12 @@ UNION SELECT about, fullname, email, nickname FROM users AS u2
     ON u2.nickname = p.author
     where lower(p.forum) = lower($1)
     ) as res `
-	//fmt.Println(limit)
-
 	if since != "" {
 		if desc {
 			query += ` where lower(nickname) < lower($2) COLLATE "ucs_basic" `
 		} else {
 			query += ` where lower(nickname) > lower($2) COLLATE "ucs_basic" `
 		}
-
 	}
 	query += `ORDER BY lower(nickname) COLLATE "ucs_basic" `
 	if desc {
@@ -214,28 +163,23 @@ UNION SELECT about, fullname, email, nickname FROM users AS u2
 	if limit != "" {
 		query += " limit " + limit
 	}
-	//fmt.Println(query)
 	var resultRows *sql.Rows
 	var err error
 	if since != "" {
 		resultRows, err = Db.Query(query, forum, since)
-
 	} else {
 		resultRows, err = Db.Query(query, forum)
 	}
-
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer resultRows.Close()
-
 	for resultRows.Next() {
 		user := new(Models.User)
 		err := resultRows.Scan(&user.About, &user.Fullname, &user.Email, &user.Nickname)
 		if err != nil {
 			panic(err)
 		}
-
 		users = append(users, *user)
 	}
 	forums:=GetForumBySlug(Db,forum)
@@ -243,11 +187,8 @@ UNION SELECT about, fullname, email, nickname FROM users AS u2
 		respWriter.WriteHeader(http.StatusNotFound)
 		tmp:=errr{"Can't find user by nickname: "+forum}
 		writeJSONBody(&respWriter, tmp)
-
 	}else {
 		respWriter.WriteHeader(http.StatusOK)
 		writeJSONBody(&respWriter, users)
-
-
 	}
 }
